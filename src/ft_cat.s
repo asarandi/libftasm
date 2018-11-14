df625b02af14872e948f74ae23a6f209488605c4
Alexandr Sarandi <asarandi@student.42.us.org>
2018-11-14 10:06:36 -0800

    replaced syscalls in ft_cat.s with regular functions

--- src/ft_cat.s
+++ src/ft_cat.s
@@ -1,13 +1,11 @@
 global _ft_cat
 
-SYS_read	equ	0x2000003	;macos64 specific
-SYS_write	equ	0x2000004	;macos64 specific
+extern	_read, _write
 
 section .data
 	buf:	times 1024	db 0
 
 section .text
-
 _ft_cat:
 	push	rbp
 	mov		rbp, rsp
@@ -16,16 +14,14 @@ _ft_cat:
 _ft_cat_loop:
 	lea		rsi, [rel buf]
 	mov		rdx, 1024
-	mov		rax, SYS_read
-	syscall
+	call	_read
 	cmp		rax, 1
 	jl		_ft_cat_return	; 0 = eof, -1 = error
 	mov		rdx, rax
 	push	rdi				;save file descriptor for reading
 	mov		rdi, 1
 	lea		rsi, [rel buf]
-	mov		rax, SYS_write
-	syscall
+	call	_write
 	pop		rdi				;restore fd for reading
 	cmp		rax, -1			;failed to write?
 	jnz		_ft_cat_loop
